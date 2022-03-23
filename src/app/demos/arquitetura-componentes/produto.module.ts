ed69c2e6f4bec5054641f77cf2a4b72253497176
OruamC <oru_catani@hotmail.com>
2022-03-22 21:30:01 -0300

    lazy load product module

---
+++ src/app/demos/arquitetura-componentes/produto.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ProdutoDashboardComponent } from './produto-dashboard/produto-dashboard.component';
+import { ProdutoRoutingModule } from './produto.route';
+
+@NgModule({
+  declarations: [ProdutoDashboardComponent],
+  imports: [CommonModule, ProdutoRoutingModule],
+  exports: [],
+})
+export class ProdutoModule {}
