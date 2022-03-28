b2eed1750d02bf354cf1f0c739899faab80c9010
OruamC <oru_catani@hotmail.com>
2022-03-28 20:56:52 -0300

    Usando ng zones

--- src/app/demos/bar-di-zones/bar.component.ts
+++ src/app/demos/bar-di-zones/bar.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, Inject, Injector, OnInit } from '@angular/core';
+import { Component, Inject, Injector, NgZone, OnInit } from '@angular/core';
 import { BarUnidadeConfig, BAR_UNIDADE_CONFIG } from './bar.config';
 import { BarFactory, BarServices, BebidaService } from './bar.service';
 
@@ -27,7 +27,8 @@ export class BarComponent implements OnInit {
     private barServices: BarServices,
     @Inject('ConfigManualUnidade') private apiConfigManual: BarUnidadeConfig,
     @Inject(BAR_UNIDADE_CONFIG) private apiConfig: BarUnidadeConfig,
-    private bebidaService: BebidaService
+    private bebidaService: BebidaService,
+    private ngZone: NgZone
   ) {}
 
   ngOnInit() {
@@ -37,5 +38,37 @@ export class BarComponent implements OnInit {
     this.dadosUnidade = this.barServices.obterUnidade();
 
     this.barBebida2 = this.bebidaService.obterBebidas();
+  }
+
+  public progress: number = 0;
+  public label: string;
+
+  processWithinAngularZone() {
+    this.label = 'dentro';
+    this.progress = 0;
+    this._increaseProgress(() =>
+      console.log('Finalizado por dentro do Angular!')
+    );
+  }
+
+  processOutsideOfAngularZone() {
+    this.label = 'fora';
+    this.progress = 0;
+    this.ngZone.runOutsideAngular(() => {
+      this._increaseProgress(() => {
+        this.ngZone.run(() => console.log('Finalizado por fora do Angular!'));
+      });
+    });
+  }
+
+  _increaseProgress(doneCallback: () => void) {
+    this.progress += 1;
+    console.log(`Progresso atual: ${this.progress}%`);
+
+    if (this.progress < 100) {
+      window.setTimeout(() => this._increaseProgress(doneCallback), 10);
+    } else {
+      doneCallback();
+    }
   }
 }
